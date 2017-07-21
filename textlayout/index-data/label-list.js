bcd63451d064a6f7f3a72d34649e318c6f1e912a
r12a <ishida@w3.org>
2017-07-21 14:05:08 +0100

    textlayout tracker, new tracks info-type-request, spec-type-issue, browser-type-bug issues

---
+++ textlayout/index-data/label-list.js
@@ -0,0 +1,28 @@
+// Full list of labels in github to use as headers.
+// Nothing will be produced if there's no open issue for a given label.
+
+var labels = { 
+	'baselines':[],
+	'bidi-controls':[],
+	'boundaries':[],
+	'character-transforms':[],
+	'cursive-text':[],
+	'emphasis':[],
+	'fonts':[],
+	'glyphs-diacritics':[],
+	'hyphenation':[],
+	'initial-letter':[],
+	'inline-spacing':[],
+	'justification':[],
+	'line-break':[],
+	'lists':[],
+	'notes-footnotes':[],
+	'numbers':[],
+	'pagination':[],
+	'paragraph-features':[],
+	'punctuation':[],
+	'quotations':[],
+	'ruby':[],
+	'text-decoration':[],
+	'vertical-text':[],
+	}
