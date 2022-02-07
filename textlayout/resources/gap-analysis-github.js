1fceeabfe299923aad355d244295a4ef8382626b
r12a <ishida@w3.org>
2022-02-07 15:53:16 +0000

    textlayout/resources/gap-analysis-github.js: Move github link out of heading. Because of respec changes that obscured the link by the self-link to the h4 head.

--- textlayout/resources/gap-analysis-github.js
+++ textlayout/resources/gap-analysis-github.js
@@ -99,7 +99,9 @@ function buildSection (theData, sectionId, doc, repo) {
 			if (debug) console.log('labelSet:',labelSet)
 
 			out += '<section id="issue'+theData[i].number+'_'+sectionId+'">\n'
-			out += '<h4><a target="_blank" href="https://github.com/w3c/'+repo+'/issues/'+theData[i].number+'">#'+theData[i].number+'</a> '+theData[i].title+'</h4>\n'
+			out += '<h4>#'+theData[i].number+' '+theData[i].title+'</h4>\n'
+			out += `<p class="ghLink"><a target="_blank" href="https://github.com/w3c/${ repo }/issues/${ theData[i].number }">GitHub issue #${ theData[i].number }</a></p>`
+
 			out += '<p>'
 
 			var body = theData[i].body
